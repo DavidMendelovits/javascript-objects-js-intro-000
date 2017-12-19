334bd1edb36c0f2b49e811381787f60251889699
David Mendelovits <david.mendelovits@gmail.com>
2017-12-19 17:21:47 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = {key: 'value'}
 
 function updatePlaylist(playlist, artistName, songTitle) {
-    return Object.assign(playlist, {artiName: songTitle})
+    return Object.assign(playlist, {artistName: songTitle})
 }
